524f98047af8b8466225026a3268ae80d6bd1f8d
metekurt <39264030+metekurt@users.noreply.github.com>
2019-05-05 13:39:10 +0300

    Update app.js
    Initializing the Session Middleware

--- practice-app/app.js
+++ practice-app/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
 
 var app = express();
 app.use(cors()); // for all routes
@@ -9,5 +10,6 @@ var port = process.env.PORT || 8080;
 app.use(express.static(__dirname + "./front-end"));
 app.use('/login',require('./back-end/routes/login'));
 app.use('/signup',require('./back-end/routes/signup'));
+app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
