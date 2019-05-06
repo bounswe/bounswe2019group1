93db7df9908561338ff090a794d47418e71c9cf7
Emirhan Saraç <emirhan.sarac29@gmail.com>
2019-05-06 04:55:42 +0300

    #98 Feat: trading api economics written

--- practice-app/app.js
+++ practice-app/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const path = require("path");
 
 var app = express();
 app.use(cors()); // for all routes
@@ -15,10 +16,12 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + "./front-end"));
+app.use(express.static(path.join(__dirname, "front-end")));
 app.use('/login',require('./back-end/routes/login'));
 app.use('/signup',require('./back-end/routes/signup'));
 app.use('/main',require('./back-end/routes/main'));
+app.use('/news',require('./back-end/routes/news'));
+app.use('/article',require('./back-end/routes/article'));
 app.use(session({ secret: '23jfDer94b3re6ibk3d1mw8ac', resave: false, saveUninitialized: false }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
