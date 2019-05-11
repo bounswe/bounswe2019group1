c87b06a050907528593eda3aaa7e9e04093915b1
ufuky <ufukyilmaz333@gmail.com>
2019-05-12 01:25:46 +0300

    #82 Create homepage front-end

--- practice-app/back-end/routes/currencyapi.js
+++ practice-app/back-end/routes/currencyapi.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const router = express.Router();
+const app = express.Router();
 var jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const { window } = new JSDOM();
 const { document } = (new JSDOM('')).window;
+
 global.document = document;
 var $ = jQuery = require('jquery')(window);
 
@@ -11,38 +12,46 @@ var $ = jQuery = require('jquery')(window);
 endpoint = 'latest'
 access_key = 'e6bd540558679f6909d74efe2470aee6';
 
-// get the most recent exchange rates via the "latest" endpoint:
-$.ajax({
-    url: 'https://data.fixer.io/api/' + endpoint + '?access_key=' + access_key,
-    dataType: 'jsonp',
-    success: function(json) {
-        // exchange rata data is stored in json.rates
-        alert(json.rates.GBP);
-        // base currency is stored in json.base
-        alert(json.base);
-        // timestamp can be accessed in json.timestamp
-        alert(json.timestamp);
-    }
-});
+
 
 //
-router.get('/currencyapi', (req, res) => {
-
-    res.status(200).end(JSON.stringify({success: success}))
+app.get('/currencyapi', (req, res) => {
+    var x = getAjax()
+    res.end(x)
 });
 
-router.post('/currencyapi', async (req, res) => {
+app.post('/currencyapi', async (req, res) => {
 
     //console.log(req.body);
     //res.status(200);
-
     var success = await login(req.body.username, req.body.password);
     console.log(success);
     res.status(200).end(JSON.stringify({success: success}));
     //res.end(JSON.stringify({a:1, b:23}));
     //...password check
     //.. bla bla
-
     //res.status(200).redirect("./profile")
-
 });
+
+function getAjax() {
+    // get the most recent exchange rates via the "latest" endpoint:
+    return $.ajax({
+        url: 'https://data.fixer.io/api/' + endpoint + '?access_key=' + access_key,
+        dataType: 'jsonp',
+        success: function(json) {
+            // exchange rata data is stored in json.rates
+            alert(json.rates.GBP);
+            // base currency is stored in json.base
+            alert(json.base);
+            // timestamp can be accessed in json.timestamp
+            alert(json.timestamp);
+        },
+        error: function( req, status, err ) {
+            console.log( 'something went wrong', status, err );
+        }
+    });
+
+}
+
+
+
