6aaee62e788049da3f190dd72a0819e52b34bf61
irem-ustunboyacioglu <32355590+irem-ustunboyacioglu@users.noreply.github.com>
2019-05-12 15:21:07 +0300

    #77 Feat: A new API is added (getBFX)

--- practice-app/app.js
+++ practice-app/app.js
@@ -13,5 +13,6 @@ var port = process.env.PORT || 8080;
 app.use(express.static(__dirname + "./front-end"));
 app.use('/login',require('./back-end/routes/login'));
 app.use('/signup',require('./back-end/routes/signup'));
+app.use('/bitfinex',require('./back-end/routes/getBfx'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
