4a2a03d7c5df5c4927ae42599a89a7b23efb4c09
abdullah-coskun <abdullah.coskun1@boun.edu.tr>
2019-10-02 10:11:25 +0300

    profile and register url

--- backend/tradersplatform/myuser/serializers.py
+++ backend/tradersplatform/myuser/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework.serializers import ModelSerializer
+
+from auth.serializers import UserListSerializer
 from .models import TemplateUser
 from rest_framework.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
+from django.contrib.auth import authenticate
+from rest_framework.fields import CharField
+from rest_framework_jwt.settings import api_settings
+
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 
 class TempUserCreateSerializer(ModelSerializer):
@@ -21,3 +29,47 @@ class TempUserCreateSerializer(ModelSerializer):
             'iban_number',
             'citizenship_number',
         ]
+        extra_kwargs = {"password": {"write_only": True,"required": False},
+                        "phone_number": {"required": False},
+                        "location": {"required": False},
+                        "iban_number":{"required":True},
+                        "citizenship_number":{"required":True},
+                        }
+
+
+class TempUserLoginSerializer(ModelSerializer):
+    token = CharField(allow_blank=True, read_only=True)
+    username = CharField(write_only=True, required=True)
+    user = UserListSerializer(read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'password',
+            'token',
+            'user',
+
+        ]
+        extra_kwargs = {"password":
+                            {"write_only": True}
+                        }
+
+    def validate(self, data):
+        password = data.get("password")
+        username = data.get("username")
+        user = TemplateUser.objects.filter(username=username).distinct()
+        user2 = authenticate(username=username, password=password)
+        if user.exists() and user.count() == 1:
+            user_obj = user.first()
+        else:
+            raise ValidationError("Incorrect credential")
+        if user_obj:
+            if not user_obj.check_password(password):
+                raise ValidationError("Incorrect credential")
+
+        payload = jwt_payload_handler(user_obj)
+        token = jwt_encode_handler(payload)
+        data["token"] = token
+        data["user"] = user_obj
+        return data
--- backend/tradersplatform/myuser/urls.py
+++ backend/tradersplatform/myuser/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^registertrader/', views.TempUserCreateAPIView.as_view(), name="register"),
     url(r'^registerbasic/', views.TempUserCreateAPIView.as_view(), name="register"),
+    url(r'^login/', views.UserLoginAPIView.as_view(), name="register"),
+    url(r'^profile/', views.UserAutoLogin.as_view(), name="register"),
 ]
