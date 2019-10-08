3fcdfbcbac033b7cd5d4229a148b8f676be0cba6
ufuky <ufukyilmaz333@gmail.com>
2019-10-08 20:12:49 +0300

    Add material kit react components such as Button, Card, Header, Footer etc. Create initial version of login and profile pages.

--- practice-app/react-front-end/src/index.js
+++ practice-app/react-front-end/src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+//import * as serviceWorker from './serviceWorker';
+import "assets/scss/material-kit-react.scss?v=1.8.0";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// pages for this product
+import ProfilePage from "views/ProfilePage/ProfilePage.js";
+import LoginPage from "views/LoginPage/LoginPage.js";
+
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/profile-page" component={ProfilePage} />
+      <Route path="/login-page" component={LoginPage} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
