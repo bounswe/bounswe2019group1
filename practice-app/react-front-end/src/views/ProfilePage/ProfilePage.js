65fa816c6b74a82e1fe557f582658666f4f67796
ufuky <ufukyilmaz333@gmail.com>
2019-10-08 21:46:29 +0300

    Corrected errors after component integration.

--- practice-app/react-front-end/src/views/ProfilePage/ProfilePage.js
+++ practice-app/react-front-end/src/views/ProfilePage/ProfilePage.js
@@ -8,29 +8,29 @@ import Camera from "@material-ui/icons/Camera";
 import Palette from "@material-ui/icons/Palette";
 import Favorite from "@material-ui/icons/Favorite";
 // core components
-import Header from "../../components/Header/Header.js";
-import Footer from "../../components/Footer/Footer.js";
-import Button from "../../components/CustomButtons/Button.js";
-import GridContainer from "../../components/Grid/GridContainer.js";
-import GridItem from "../../components/Grid/GridItem.js";
-import HeaderLinks from "../../components/Header/HeaderLinks.js";
-import NavPills from "../../components/NavPills/NavPills.js";
-import Parallax from "../../components/Parallax/Parallax.js";
+import Header from "components/Header/Header.js";
+import Footer from "components/Footer/Footer.js";
+import Button from "components/CustomButtons/Button.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import NavPills from "components/NavPills/NavPills.js";
+import Parallax from "components/Parallax/Parallax.js";
 
-import profile from "../../assets/img/faces/christian.jpg";
+import profile from "assets/img/faces/christian.jpg";
 
-import studio1 from "../../assets/img/examples/studio-1.jpg";
-import studio2 from "../../assets/img/examples/studio-2.jpg";
-import studio3 from "../../assets/img/examples/studio-3.jpg";
-import studio4 from "../../assets/img/examples/studio-4.jpg";
-import studio5 from "../../assets/img/examples/studio-5.jpg";
-import work1 from "../../assets/img/examples/olu-eletu.jpg";
-import work2 from "../../assets/img/examples/clem-onojeghuo.jpg";
-import work3 from "../../assets/img/examples/cynthia-del-rio.jpg";
-import work4 from "../../assets/img/examples/mariya-georgieva.jpg";
-import work5 from "../../assets/img/examples/clem-onojegaw.jpg";
+import studio1 from "assets/img/examples/studio-1.jpg";
+import studio2 from "assets/img/examples/studio-2.jpg";
+import studio3 from "assets/img/examples/studio-3.jpg";
+import studio4 from "assets/img/examples/studio-4.jpg";
+import studio5 from "assets/img/examples/studio-5.jpg";
+import work1 from "assets/img/examples/olu-eletu.jpg";
+import work2 from "assets/img/examples/clem-onojeghuo.jpg";
+import work3 from "assets/img/examples/cynthia-del-rio.jpg";
+import work4 from "assets/img/examples/mariya-georgieva.jpg";
+import work5 from "assets/img/examples/clem-onojegaw.jpg";
 
-import styles from "../../assets/jss/material-kit-react/views/profilePage.js";
+import styles from "assets/jss/material-kit-react/views/profilePage.js";
 
 const useStyles = makeStyles(styles);
 
@@ -43,6 +43,7 @@ export default function ProfilePage(props) {
     classes.imgFluid
   );
   const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
+  console.log("hey haha")
   return (
     <div>
       <Header
@@ -59,7 +60,7 @@ export default function ProfilePage(props) {
       <Parallax
         small
         filter
-        image={require("../../assets/img/profile-bg.jpg")}
+        image={require("assets/img/profile-bg.jpg")}
       />
       <div className={classNames(classes.main, classes.mainRaised)}>
         <div>
