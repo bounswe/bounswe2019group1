ae0f4829b9e667692bb42f8b2b7cd0ace8cac13a
metekurt <rjtmjx@gmail.com>
2019-10-11 12:42:57 +0300

    SignupPage is implemented

---
+++ practice-app/react-front-end/src/views/SignupPage/SignupPage.js
@@ -0,0 +1,194 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Icon from "@material-ui/core/Icon";
+// @material-ui/icons
+import Email from "@material-ui/icons/Email";
+// core components
+import Header from "components/Header/Header.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import Footer from "components/Footer/Footer.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
+import Card from "components/Card/Card.js";
+import CardBody from "components/Card/CardBody.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardFooter from "components/Card/CardFooter.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import FormControl from '@material-ui/core/FormControl';
+import styles from "assets/jss/material-kit-react/views/loginPage.js";
+import { Select } from "@material-ui/core";
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import image from "assets/img/bg7.jpg";
+const useStyles = makeStyles(styles);
+export default function SignupPage(props) {
+    const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
+    setTimeout(function() {
+      setCardAnimation("");
+    }, 700);
+    const classes = useStyles();
+    const { ...rest } = props;
+    return (
+      <div>
+        <Header
+          absolute
+          color="transparent"
+          brand="Traders Platform"
+          rightLinks={<HeaderLinks />}
+          {...rest}
+        />
+        <div
+          className={classes.pageHeader}
+          style={{
+            backgroundImage: "url(" + image + ")",
+            backgroundSize: "cover",
+            backgroundPosition: "top center"
+          }}
+        >
+          <div className={classes.container}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={4}>
+                <Card className={classes[cardAnimaton]}>
+                  <form className={classes.form}>
+                    <CardHeader color="primary" className={classes.cardHeader}>
+                      <h4>Create an Account</h4>
+                      <div className={classes.socialLine}>
+                        
+                        
+                        <Button
+                          justIcon
+                          href="#pablo"
+                          target="_blank"
+                          color="transparent"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className={"fab fa-google-plus-g"} />
+                        </Button>
+                      </div>
+                    </CardHeader>
+                    <p className={classes.divider}>Or Be Classical</p>
+                    <CardBody>
+                      <CustomInput
+                        labelText="First Name..."
+                        id="first"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          type: "text",
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <Icon className={classes.inputIconsColor}>
+                              perm_identity
+                              </Icon>
+                            </InputAdornment>
+                          )
+                        }}
+                      />
+                      <CustomInput
+                        labelText="Email..."
+                        id="email"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          type: "email",
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <Email className={classes.inputIconsColor} />
+                            </InputAdornment>
+                          )
+                        }}
+                      />
+                      <CustomInput
+                        labelText="Password"
+                        id="pass"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          type: "password",
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <Icon className={classes.inputIconsColor}>
+                                lock_outline
+                              </Icon>
+                            </InputAdornment>
+                          ),
+                          autoComplete: "off"
+                        }}
+                      />
+  
+                      <CustomInput
+                        labelText="Location..."
+                        id="location"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          type: "location",
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <Icon className={classes.inputIconsColor}>
+                              room
+                              </Icon>
+                            </InputAdornment>
+                          )
+                        }}
+                      />
+                      
+                     {/* <CustomInput
+                        labelText="Select User Type..."
+                        id="email"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        
+                        inputProps={{
+                          type: "email",
+                          endAdornment: (
+                            <InputAdornment position="end">
+                             <Icon className={classes.inputIconsColor}>
+                              people
+                              </Icon>
+                            </InputAdornment>
+                          )
+                        }}
+                        
+                      />*/}
+                    
+                    
+                      
+                    
+          <FormControl className={classes.formControl}>
+          <Select >
+            <MenuItem value={10}>Basic</MenuItem>
+            <MenuItem value={20}>Trading</MenuItem>
+            
+          </Select>
+          <FormHelperText>Select User Type</FormHelperText>   
+          </FormControl>
+        
+  
+      
+        
+          </CardBody>
+                    
+                    <CardFooter className={classes.cardFooter}>
+                      <Button simple color="primary" size="lg">
+                        Confirm
+                      </Button>
+                    </CardFooter>
+                  </form>
+                </Card>
+              </GridItem>
+            </GridContainer>
+          </div>
+          <Footer whiteFont />
+        </div>
+      </div>
+    );
+  }
