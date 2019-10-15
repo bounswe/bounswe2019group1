0b9131da80397ee70d234093c88051006e54045a
altay <altayince34@gmail.com>
2019-10-15 21:57:33 +0300

    Home page -eksikleri var-

--- practice-app/react-front-end/src/App.js
+++ practice-app/react-front-end/src/App.js
@@ -1,26 +1,179 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Icon from "@material-ui/core/Icon";
+// @material-ui/icons
+import Email from "@material-ui/icons/Email";
+// core components
+import Header from "components/Header/Header.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import Footer from "components/Footer/Footer.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
+import Card from "components/Card/Card.js";
+import CardBody from "components/Card/CardBody.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardFooter from "components/Card/CardFooter.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import { Link } from "react-router-dom";
 
-function App() {
+
+
+
+import styles from "assets/jss/material-kit-react/views/loginPage.js";
+
+import image from "assets/img/dollar-hd.jpg";
+const useStyles = makeStyles(styles);
+export default function LoginPage(props) {
+  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
+  setTimeout(function() {
+    setCardAnimation("");
+  }, 700);
+  const classes = useStyles();
+  const { ...rest } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+    <div>
+      <Header
+        absolute
+        color="transparent"
+        brand="Khaji-it Traders Platform"
+        rightLinks={<HeaderLinks />}
+        {...rest}
+      />
+      <div
+        className={classes.pageHeader}
+        style={{
+          backgroundImage: "url(" + image + ")",
+          backgroundSize: "cover",
+          backgroundPosition: "top center"
+        }}
         >
-          Learn React
-        </a>
-      </header>
+        <div className={classes.container}>
+          <GridContainer justify="center">
+            <GridItem xs={12} sm={12} md={4}>
+              <Card className={classes[cardAnimaton]}>
+                <form className={classes.form}>
+                  <CardHeader color="primary" className={classes.cardHeader}>
+                    <h4>Login</h4>
+                    <div className={classes.socialLine}>
+                      <Button
+                        justIcon
+                        href="#pablo"
+                        target="_blank"
+                        color="transparent"
+                        onClick={e => e.preventDefault()}
+                      >
+                        <i className={"fab fa-google-plus-g"} />
+                      </Button>
+                    </div>
+                  </CardHeader>
+                  <CardBody>
+                    <CustomInput
+                      labelText="Email..."
+                      id="email"
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                      inputProps={{
+                        type: "email",
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <Email className={classes.inputIconsColor} />
+                          </InputAdornment>
+                        )
+                      }}
+                    />
+                    <CustomInput
+                      labelText="Password"
+                      id="pass"
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                      inputProps={{
+                        type: "password",
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <Icon className={classes.inputIconsColor}>
+                              lock_outline
+                            </Icon>
+                          </InputAdornment>
+                        ),
+                        autoComplete: "off"
+                      }}
+                    />
+                  </CardBody>
+                  <CardFooter className={classes.cardFooter}>
+                    <Button simple color="primary" size="lg">
+                      Login
+                    </Button>
+                  </CardFooter>
+                  <CardFooter className={classes.cardFooter}>
+                    New to Khaji-it?
+                    <Link to="/sign-up">
+                      Sign up now
+                    </Link>
+                  </CardFooter>
+                </form>
+              </Card>
+                  <CustomInput
+                  labelText="Read financial news the fastest and innovative way!"
+                  id="pass"
+                  formControlProps={{
+                    fullWidth: true
+                  }}
+                  inputProps={{
+                    endAdornment: (
+                      <InputAdornment position='start'>
+                        <Icon className={classes.homePageItem}>
+                        trending_up
+                        </Icon>
+                      </InputAdornment>
+                    ),
+                    autoComplete: "off"
+                  }}
+                  />
+                  <CustomInput
+                  labelText="Build your own profile & follow other traders!"
+                  id="pass"
+                  formControlProps={{
+                    fullWidth: true
+                  }}
+                  inputProps={{
+                    endAdornment: (
+                      <InputAdornment position='start'>
+                        <Icon className={classes.homePageItem}>
+                        assignment_ind
+                        </Icon>
+                      </InputAdornment>
+                    ),
+                    autoComplete: "off"
+                  }}
+                  />
+                  <CustomInput
+                  labelText="Get the androidd app now!"
+                  id="pass"
+                  formControlProps={{
+                    fullWidth: true
+                  }}
+                  inputProps={{
+                    endAdornment: (
+                      <InputAdornment position='start'>
+                        <Icon className={classes.homePageItem}>
+                        android
+                        </Icon>
+                      </InputAdornment>
+                    ),
+                    autoComplete: "off"
+                  }}
+                  />
+            </GridItem>
+          </GridContainer>
+        </div>
+        <Footer whiteFont />
+      </div>
     </div>
   );
 }
-
-export default App;
