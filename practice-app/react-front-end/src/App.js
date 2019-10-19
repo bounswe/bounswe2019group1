f282f15c23c806b5a1efcc3f0c2d4cc22246cbba
Ufuk Yilmaz <ufukyilmaz333@gmail.com>
2019-10-19 21:03:20 +0300

    Added getting profile info logic and remove unnecessary imports.

--- practice-app/react-front-end/src/App.js
+++ practice-app/react-front-end/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import Icon from "@material-ui/core/Icon";
+
 // @material-ui/icons
-import Email from "@material-ui/icons/Email";
+
 // core components
 import Header from "components/Header/Header.js";
 import HeaderLinks from "components/Header/HeaderLinks.js";
@@ -13,10 +12,6 @@ import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import Button from "components/CustomButtons/Button.js";
 import Card from "components/Card/Card.js";
-import CardBody from "components/Card/CardBody.js";
-import CardHeader from "components/Card/CardHeader.js";
-import CardFooter from "components/Card/CardFooter.js";
-import CustomInput from "components/CustomInput/CustomInput.js";
 
 import styles from "assets/jss/material-kit-react/views/loginPage.js";
 
@@ -34,10 +29,6 @@ import { Link } from "react-router-dom";
 
 const useStyles = makeStyles(styles);
 export default function LoginPage(props) {
-  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
-  setTimeout(function() {
-    setCardAnimation("");
-  }, 700);
   const classes = useStyles();
   const { ...rest } = props;
   return (
