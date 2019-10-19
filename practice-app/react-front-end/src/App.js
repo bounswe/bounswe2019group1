cefe014d0df603ea2e441ec64401113491537bef
Ufuk Yilmaz <ufukyilmaz333@gmail.com>
2019-10-19 20:40:56 +0300

    Fix the links and change the brand label.

--- practice-app/react-front-end/src/App.js
+++ practice-app/react-front-end/src/App.js
@@ -21,18 +21,16 @@ import CustomInput from "components/CustomInput/CustomInput.js";
 import styles from "assets/jss/material-kit-react/views/loginPage.js";
 
 //Altay home page importlari.
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 
 import image from "assets/img/dollar-hd.jpg";
 import cardimage from "assets/img/pricehistory.jpg";
 import cardimage2 from "assets/img/handshake.jpg";
-
-
-
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles(styles);
 export default function LoginPage(props) {
@@ -47,7 +45,7 @@ export default function LoginPage(props) {
       <Header
         absolute
         color="transparent"
-        brand="Material Kit React"
+        brand="Khaji-it Traders Platform"
         rightLinks={<HeaderLinks />}
         {...rest}
       />
@@ -62,66 +60,76 @@ export default function LoginPage(props) {
         <div className={classes.container}>
           <GridContainer justify="center">
             <GridItem xs={12} sm={12} md={4}>
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  alt="Contemplative Reptile"
-                  height="140"
-                  image={cardimage}
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    <center>Welcome</center>
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    Login to check trending financial news.
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button
-                size="small"
-                color="primary"
-                href="http://localhost:3000/login-page"
-                className={classes.button}>
-                  Login
-                </Button>
-              </CardActions>
-            </Card>
-            <Card className={classes.card2}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  alt="Contemplative Reptile"
-                  height="140"
-                  image={cardimage2}
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    <center>Not a member?</center>
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    Sign up to become a member of Khaji-it family.
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button
-                size="small"
-                color="primary"
-                href="http://localhost:3000/sign-up"
-                className={classes.button}
-                >
-                Sign Up
-                </Button>
-              </CardActions>
-            </Card>
+              <Card className={classes.card}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="140"
+                    image={cardimage}
+                    title="Contemplative Reptile"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      <center>Welcome</center>
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      Login to check trending financial news.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Link to="/login-page">
+                    <Button
+                      size="small"
+                      color="primary"
+                      className={classes.button}
+                    >
+                      Login
+                    </Button>
+                  </Link>
+                </CardActions>
+              </Card>
+              <Card className={classes.card2}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="140"
+                    image={cardimage2}
+                    title="Contemplative Reptile"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      <center>Not a member?</center>
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      Sign up to become a member of Khaji-it family.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Link to="/sign-up">
+                    <Button
+                      size="small"
+                      color="primary"
+                      className={classes.button}
+                    >
+                      Sign Up
+                    </Button>
+                  </Link>
+                </CardActions>
+              </Card>
             </GridItem>
           </GridContainer>
-
         </div>
         <Footer whiteFont />
       </div>
