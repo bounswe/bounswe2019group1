c86974f656fbde47cfc62ca981e3284bc0687188
abdullah-coskun <abdullah.coskun1@boun.edu.tr>
2019-10-21 20:55:44 +0300

    retrieve for mobile

--- backend/tradersplatform/myuser/urls.py
+++ backend/tradersplatform/myuser/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^login/', views.UserLoginAPIView.as_view(), name="register"),
     url(r'^profile/', views.UserAutoLogin.as_view(), name="register"),
     url(r'^retrieve/(?P<pk>[0-9_]+)/', views.UserRetrieveAPI.as_view(), name="register"),
+    url(r'^retrievemobile/', views.UserRetrieveMobileAPI.as_view(), name="register"),
     url(r'^search_user/', views.SearchUserListAPIView.as_view(), name="update_de"),
     #url(r'^logs/', views.NoParsingFilter(), name="register"),
 ]
