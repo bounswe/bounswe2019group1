a00006c5d4f39fd9ca726f5f02ee96b74c5ac46d
abdullah-coskun <abdullah.coskun1@boun.edu.tr>
2019-10-21 17:00:49 +0300

    search mechanism

--- backend/tradersplatform/myuser/urls.py
+++ backend/tradersplatform/myuser/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^registerbasic/', views.TempUserCreateAPIView.as_view(), name="register"),
     url(r'^login/', views.UserLoginAPIView.as_view(), name="register"),
     url(r'^profile/', views.UserAutoLogin.as_view(), name="register"),
+    url(r'^search_user/', views.SearchUserListAPIView.as_view(), name="update_de"),
     #url(r'^logs/', views.NoParsingFilter(), name="register"),
 ]
