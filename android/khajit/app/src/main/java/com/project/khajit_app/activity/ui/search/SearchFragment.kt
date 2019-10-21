826f72043652177d7493e334e7f0f9ebe15f7466
Emirhan Saraç <emirhan.sarac29@gmail.com>
2019-10-21 17:51:15 +0300

    #218 editing page problem solved

--- android/khajit/app/src/main/java/com/project/khajit_app/activity/ui/search/SearchFragment.kt
+++ android/khajit/app/src/main/java/com/project/khajit_app/activity/ui/search/SearchFragment.kt
@@ -1,13 +1,17 @@
 package com.project.khajit_app.activity.ui.search
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.project.khajit_app.R
+import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment() {
 
@@ -25,6 +29,23 @@ class SearchFragment : Fragment() {
 
         var loader = root.findViewById(R.id.progress_loader) as ProgressBar
         loader.visibility = View.GONE
+
+
+        var searchview = root.findViewById(R.id.search) as SearchView
+        searchview.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(root.context, "Signed in, idToken: ", Toast.LENGTH_SHORT).show()
+                return false
+            }
+
+        })
+
+
         return root
     }
 }
