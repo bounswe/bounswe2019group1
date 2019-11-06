a23501ed16375c363116fa336e152247d1b4695a
abdullah-coskun <abdullah.coskun1@boun.edu.tr>
2019-11-06 12:25:01 +0300

    basic coinlayer  cryptocurrency request

--- backend/tradersplatform/myuser/urls.py
+++ backend/tradersplatform/myuser/urls.py
@@ -17,5 +17,6 @@ urlpatterns = [
     url(r'^logarrange/', views.ArrangeLog.as_view(), name="update_de"),
     url(r'^forgotpassword/', views.ForgotPassword.as_view(), name="update_de"),
     url(r'^currency/', views.CurrencyAPI.as_view(), name="update_de"),
+    url(r'^cryptocurrency/', views.CryptoCurrencyAPI.as_view(), name="update_de"),
     #url(r'^logs/', views.NoParsingFilter(), name="register"),
 ]
