b1175e05f2a701bac7f0035758ccefd0ffdad5f9
abdullah-coskun <abdullah.coskun1@boun.edu.tr>
2019-11-13 16:47:08 +0300

    ETF update and complete currency complete

--- backend/tradersplatform/equipment/models.py
+++ backend/tradersplatform/equipment/models.py
@@ -20,3 +20,23 @@ class Stocks(models.Model):
     GOOGL = models.CharField(max_length=300, blank=True,null=True, unique=False)
     AAPL = models.CharField(max_length=300, blank=False, null=False, unique=False)
     GM = models.CharField(max_length=300, blank=False, null=False, unique=False)
+
+
+class Currencies(models.Model):
+    EUR = models.CharField(max_length=300, blank=True,null=True, unique=False)
+    GBP = models.CharField(max_length=300, blank=False, null=False, unique=False)
+    TRY = models.CharField(max_length=300, blank=False, null=False, unique=False)
+
+
+class ETFDetail(models.Model):
+    mobile_title = models.CharField(max_length=800, blank=True, null=True, unique=False)
+    price = models.CharField(max_length=800, blank=True, null=True, unique=False)
+    assets = models.CharField(max_length=800, blank=True, null=True, unique=False)
+    average_volume = models.CharField(max_length=800, blank=True, null=True, unique=False)
+    ytd = models.CharField(max_length=800, blank=True, null=True, unique=False)
+
+
+class ETFs(models.Model):
+    SPY = models.ForeignKey(ETFDetail, on_delete=models.CASCADE, default='',related_name='SPY')
+    IVV = models.ForeignKey(ETFDetail, on_delete=models.CASCADE, default='',related_name='IVV')
+    VTI = models.ForeignKey(ETFDetail, on_delete=models.CASCADE, default='',related_name='VTI')
