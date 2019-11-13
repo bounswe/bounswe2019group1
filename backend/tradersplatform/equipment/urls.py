bc8bf23c6cbbeba5b0aff3a9c8f37ccb0354094f
abdullah-coskun <abdullah.coskun1@boun.edu.tr>
2019-11-13 20:13:26 +0300

    gainer trace indices

--- backend/tradersplatform/equipment/urls.py
+++ backend/tradersplatform/equipment/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^metalcurrency/', views.MetalCurrencyAPI.as_view(), name="update_de"),
     url(r'^stock/', views.StockCurrencyAPI.as_view(), name="update_de"),
     url(r'^traceindices/', views.TraceIndices.as_view(), name="update_de"),
+    url(r'^traceindicesgainers/', views.TraceIndicesGainers.as_view(), name="update_de"),
     url(r'^etfs/', views.ETFsListAPIView.as_view(), name="update_de"),
     url(r'^bonds/', views.BondListAPIView.as_view(), name="update_de"),
     url(r'^lastmonth/', views.StockLastMonth.as_view(), name="update_de"),
