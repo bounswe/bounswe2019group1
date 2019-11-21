67b272cf916b6d6208d09cb2c891c70919717545
Emirhan Saraç <emirhan.sarac29@gmail.com>
2019-11-22 02:52:30 +0300

    #300 .feat search now results name and title

--- android/khajit/app/src/main/java/com/project/khajit_app/activity/ui/search/SearchFragment.kt
+++ android/khajit/app/src/main/java/com/project/khajit_app/activity/ui/search/SearchFragment.kt
@@ -2,21 +2,43 @@ package com.project.khajit_app.activity.ui.search
 
 
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
-import android.widget.SearchView
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProviders
 import com.project.khajit_app.R
+import com.project.khajit_app.activity.HomeFeedPageActivity
+import com.project.khajit_app.activity.ListViewAdapter
+import com.project.khajit_app.activity.UserViewAdapter
+import com.project.khajit_app.activity.ui.home.HomeFragment
+import com.project.khajit_app.api.RetrofitClient
+import com.project.khajit_app.data.models.FollowingModel
+import com.project.khajit_app.data.models.SearchRequest
+import com.project.khajit_app.data.models.SearchResponse
+import com.project.khajit_app.global.User
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class SearchFragment : Fragment() {
 
     private lateinit var searchViewModel: SearchViewModel
+
+    private var list_usernames = arrayListOf<String>()
+    private var list_titles = arrayListOf<String>()
+    private var list_ids = arrayListOf<Int>()
+
+    lateinit var ladapter: UserViewAdapter
+    lateinit var lview: ListView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,8 +53,6 @@ class SearchFragment : Fragment() {
         var loader = root.findViewById(R.id.progress_loader) as ProgressBar
         loader.visibility = View.GONE
 
-
-
         var searchview = root.findViewById(R.id.search) as SearchView
         searchview.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
 
@@ -41,12 +61,61 @@ class SearchFragment : Fragment() {
             }
 
             override fun onQueryTextSubmit(query: String): Boolean {
-                Toast.makeText(root.context, "Signed in, idToken: ", Toast.LENGTH_SHORT).show()
+                var search_query = SearchRequest(query)
+                RetrofitClient.instance.searchUsername(search_query).enqueue(object :
+                    Callback<SearchResponse> {
+                    override fun onResponse(
+                        call: Call<SearchResponse>,
+                        response: Response<SearchResponse>
+                    ) {
+                        println(response.toString())
+                        if(response.code() == 200 ){
+
+                            list_usernames.clear()
+                            list_titles.clear()
+                            list_ids.clear()
+
+                            var count = response.body()?.count
+
+                            for (a in 1..count!!) {
+                                list_usernames.add(response.body()?.results?.get(a-1)!!.first_name + " " + response.body()?.results?.get(a-1)!!.last_name)
+                                list_titles.add(response.body()?.results?.get(a-1)!!.title)
+                                list_ids.add(response.body()?.results?.get(a-1)!!.id)
+                            }
+
+                            // This will be used for further methods in order to set prediction rates
+                            lview =  root.findViewById(R.id.list_users) as ListView
+                            ladapter = UserViewAdapter(this@SearchFragment, list_usernames, list_titles)
+                            lview.adapter = ladapter
+
+                        }else{
+                            Log.d("error message:", response.message())
+                        }
+                    }
+                    override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
+                        println(t.message)
+                        println(t)
+                        Toast.makeText(context,t.message,Toast.LENGTH_LONG).show()
+                    }
+                })
                 return false
             }
 
         })
 
+        /*
+        var listview = root.findViewById(R.id.list_users) as ListView
+        listview.setOnItemClickListener{ parent, view, position, id ->
+            Toast.makeText(this.context, "text is " + " $position", Toast.LENGTH_LONG).show()
+            //<var ite = ladapter!!.getItem(position) as String
+            User.whereIamAsId = list_ids[position]
+
+            var intent = Intent(getActivity(), HomeFeedPageActivity::class.java)
+            startActivity(intent)
+        }
+        */
+
         return root
     }
+
 }
