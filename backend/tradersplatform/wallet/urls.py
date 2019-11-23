888907d89fba187ce99f816b2f26e1e7bc080209
abdullah-coskun <abdullah.coskun1@boun.edu.tr>
2019-11-23 22:54:03 +0300

    purchase equipment

--- backend/tradersplatform/wallet/urls.py
+++ backend/tradersplatform/wallet/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^createWallet/', views.WalletCreateAPIView.as_view(), name="list"),
     url(r'^ListAllWallets/', views.WalletListAPIView.as_view(), name="list"),
     url(r'^sendUSD/', views.SendUSDAPIView.as_view(), name="list"),
+    url(r'^takeequipment/', views.PurchaseEquipmentAPIView.as_view(), name="list"),
 ]
--- backend/tradersplatform/wallet/views.py
+++ backend/tradersplatform/wallet/views.py
@@ -5,11 +5,14 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView
 
+from equipment.models import *
 from follow.views import check_if_user
 from wallet.models import Wallet
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from wallet.serializers import WalletCreateSerializer, WalletListSerializer
+from django.db import models
+from django.apps import apps
 
 
 class WalletCreateAPIView(CreateAPIView):
@@ -57,6 +60,68 @@ class PurchaseEquipmentAPIView(UpdateAPIView):
         curr_wallet = Wallet.objects.filter(owner=request.user.id).first()
         if not curr_wallet:
             raise ValidationError({"detail": "You don't have wallet"})
+        name=request.data.get('name_of_eq',None)
+        amount=request.data.get('amount',None)
+        if amount is None or name is None:
+            raise ValidationError({"detail": "Give both amount and neme of equipment"})
+        is_float=isinstance(amount, float) or isinstance(amount, int)
+        if not is_float:
+            raise ValidationError({"detail": "Amount must be in float or int form"})
+        amount=decimal.Decimal(amount)
+        is_etf=False
+        if name== 'BTC' or name == 'LTC' or name== 'ETH' :
+            model='CryptoCurrencies'
+        elif name== 'XAG' or name == 'XAU':
+            model = 'Metals'
+        elif name== 'GOOGL' or name == 'AAPL' or name== 'GM' :
+            model = 'Stocks'
+        elif name== 'EUR' or name == 'GBP' or name== 'TRY' :
+            model = 'Currencies'
+        elif name== 'DJI' or name == 'IXIC' or name== 'INX' :
+            model = 'TraceIndices'
+        elif name== 'SPY' or name == 'IVV' or name== 'VTI' :
+            model = 'ETFs'
+            is_etf = True
+        else:
+            raise ValidationError({"detail": "Give a valid name"})
+        equipment = apps.get_model("equipment", model)
+        last=equipment.objects.all().last()
+        if not is_etf:
+            currency=getattr(last, name)
+        else:
+            if name == 'SPY':
+                currency = ETFPrice.objects.get(id=1).price
+            elif name == 'IVV':
+                currency = ETFPrice.objects.get(id=2).price
+            elif name == 'VTI':
+                currency = ETFPrice.objects.get(id=3).price
+            currency = decimal.Decimal(float(currency[1:]))
+        subtract_usd = currency * amount
+        curr_usd = curr_wallet.USD
+        if curr_usd < subtract_usd:
+            raise ValidationError({"detail": "You can not afford this amount"})
+        else:
+            curr_wallet.USD = curr_usd - subtract_usd
+            setattr(curr_wallet, name, amount)
+            curr_wallet.BTC = amount
+            curr_wallet.save()
+        serializer=WalletListSerializer(curr_wallet)
+        return Response(serializer.data, status=200)
+
+    '''        if name == 'BTC':
+                last=CryptoCurrencies.objects.all().last()
+                currency=last.BTC
+                subtract_usd=currency*amount
+                curr_usd=curr_wallet.USD
+                if curr_usd < subtract_usd:
+                    raise ValidationError({"detail": "You can not afford this amount"})
+                else:
+                    curr_wallet.USD=curr_usd-subtract_usd
+                    curr_wallet.BTC=amount
+                    curr_wallet.save()
+                print("")
+            else:
+                print("as")'''
 
 
 
