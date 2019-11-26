d74ea4617d972da01003ff8a337fc2152df39f7a
Ufuk Yilmaz <ufukyilmaz333@gmail.com>
2019-11-26 12:12:33 +0300

    Update edit profile.

--- practice-app/react-front-end/src/service/edit-profile-service.js
+++ practice-app/react-front-end/src/service/edit-profile-service.js
@@ -8,11 +8,39 @@ export function editProfile(values) {
     headers: {
       Authorization: authHeader(),
       "Content-Type": "application/json"
+    },
+    body: {
+      first_name: values.first_name,
+      last_name: values.last_name,
+      location: values.location.address,
+      title: values.title,
+      biography: values.biography
     }
   };
   return axios.put(
     `${environment.api_url}user/updateuser/`,
-    JSON.stringify(values),
-    requestOptions.headers
+    requestOptions.body,
+    {
+      headers: requestOptions.headers
+    }
+  );
+}
+export function changePassword(oldPassword, newPassword) {
+  const requestOptions = {
+    headers: {
+      Authorization: authHeader(),
+      "Content-Type": "application/json"
+    },
+    body: {
+      old_password: oldPassword,
+      new_password: newPassword
+    }
+  };
+  return axios.put(
+    `${environment.api_url}user/updateuser/`,
+    requestOptions.body,
+    {
+      headers: requestOptions.headers
+    }
   );
 }
