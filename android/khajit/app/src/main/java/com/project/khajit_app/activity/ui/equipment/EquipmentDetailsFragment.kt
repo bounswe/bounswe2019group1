7bb67b4e38c44be0f9940428acea9e6617a4e783
eray-kurtulus <erayk96@gmail.com>
2019-11-26 08:30:47 +0300

    Added equipment details fragment.
    Implemented some functionality of the equipment details fragment.
    
    Tidied up the equipment details layout.
    
    Some minor visual improvements.

---
+++ android/khajit/app/src/main/java/com/project/khajit_app/activity/ui/equipment/EquipmentDetailsFragment.kt
@@ -0,0 +1,48 @@
+package com.project.khajit_app.activity.ui.equipment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.project.khajit_app.R
+import interfaces.fragmentOperationsInterface
+
+class EquipmentDetailsFragment : Fragment(), fragmentOperationsInterface {
+
+    var containerId : ViewGroup? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreate(savedInstanceState)
+
+        val root = inflater.inflate(R.layout.fragment_equipment_details, container, false)
+        containerId = container
+
+        val equipmentName = root.findViewById(R.id.details_equipment_name) as TextView
+        equipmentName.text = arguments?.getString("equipment")
+
+        val equipmentValue = root.findViewById(R.id.details_equipment_value) as TextView
+        equipmentValue.text = "$ " + arguments?.getString("value")
+
+        return root
+    }
+
+
+    companion object {
+        fun newInstance(equipment : String, value : String): EquipmentDetailsFragment {
+            val fragmentEquipmentDetails = EquipmentDetailsFragment()
+            val args = Bundle()
+            args.putSerializable("equipment",equipment)
+            args.putSerializable("value",value)
+            fragmentEquipmentDetails.arguments = args
+            return fragmentEquipmentDetails
+        }
+
+    }
+}
--- android/khajit/app/src/main/java/com/project/khajit_app/activity/ui/equipment/EquipmentFragment.kt
+++ android/khajit/app/src/main/java/com/project/khajit_app/activity/ui/equipment/EquipmentFragment.kt
@@ -7,21 +7,26 @@ import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.project.khajit_app.R
 import com.project.khajit_app.activity.ui.home.HomeViewModel
 import com.project.khajit_app.api.RetrofitClient
 import com.project.khajit_app.data.models.*
+import interfaces.fragmentOperationsInterface
+import kotlinx.android.synthetic.main.fragment_equipment.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
-class EquipmentFragment : Fragment() {
+class EquipmentFragment : Fragment(), fragmentOperationsInterface {
 
     private lateinit var equipmentViewModel: EquipmentViewModel
+    var containerId : ViewGroup? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,6 +37,7 @@ class EquipmentFragment : Fragment() {
         equipmentViewModel =
             ViewModelProviders.of(this).get(EquipmentViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_equipment, container, false)
+        containerId = container
 
         val value_eur = root.findViewById(R.id.value_eur) as TextView
         val value_gbp = root.findViewById(R.id.value_gbp) as TextView
@@ -159,31 +165,36 @@ class EquipmentFragment : Fragment() {
 
             })
 
+        val eurCard = root.findViewById(R.id.equipment_fragment_eur_card) as CardView
+        val gbpCard = root.findViewById(R.id.equipment_fragment_gbp_card) as CardView
+        val tryCard = root.findViewById(R.id.equipment_fragment_try_card) as CardView
+        val btcCard = root.findViewById(R.id.equipment_fragment_btc_card) as CardView
+        val ethCard = root.findViewById(R.id.equipment_fragment_eth_card) as CardView
+        val ltcCard = root.findViewById(R.id.equipment_fragment_ltc_card) as CardView
+
+        //add listeners to the equipment cards
+        eurCard.setOnClickListener { goToEquipmentDetails("EUR", value_eur.text.toString()) }
+        gbpCard.setOnClickListener { goToEquipmentDetails("GBP", value_gbp.text.toString()) }
+        tryCard.setOnClickListener { goToEquipmentDetails("TRY", value_try.text.toString()) }
+        btcCard.setOnClickListener { goToEquipmentDetails("BTC", value_btc.text.toString()) }
+        ethCard.setOnClickListener { goToEquipmentDetails("ETH", value_eth.text.toString()) }
+        ltcCard.setOnClickListener { goToEquipmentDetails("LTC", value_ltc.text.toString()) }
+
         return root
     }
 
-    fun currencyEUR(view: View) {
+    fun goToEquipmentDetails(equipment : String, value : String) {
 
-    }
+        val parentActivityManager : FragmentManager = activity?.supportFragmentManager as FragmentManager
 
-    fun currencyGBP(view: View) {
-
-    }
-
-    fun currencyTRY(view: View) {
-
-    }
-
-    fun cryptoBTC(view: View) {
-
-    }
-
-    fun cryptoETH(view: View) {
-
-    }
-
-    fun cryptoLTC(view: View) {
-
+        fragmentTransaction(
+            parentActivityManager,
+            EquipmentDetailsFragment.newInstance(equipment, value),
+            (containerId!!.id),
+            true,
+            true,
+            false
+        )
     }
 
     fun commodityXAU(view: View) {
