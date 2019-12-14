08565205661b353d5b268ee71ebe395ebf8f5a36
abdullah-coskun <abdullah.coskun1@boun.edu.tr>
2019-12-14 16:51:43 +0300

    minor correction

--- backend/tradersplatform/notification/views.py
+++ backend/tradersplatform/notification/views.py
@@ -83,7 +83,6 @@ class ListSetNotificationAPIView(ListAPIView):
     queryset = SetNotification.objects.filter()
 
     def get_queryset(self, *args, **kwargs):
-        buy_order()
         check_if_user(self.request)
         user_id=self.request.user.id
         a=Notification.objects.filter(owner__id=user_id)
