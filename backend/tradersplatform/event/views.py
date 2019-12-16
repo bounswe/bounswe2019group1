c10a27949c63c8d09a949f917547b752f60522e0
Ömer F. Toptaş <omer.toptas@boun.edu.tr>
2019-12-16 20:15:35 +0300

    A little correction
    I forgot to get the searched item from the request user made.

--- backend/tradersplatform/event/views.py
+++ backend/tradersplatform/event/views.py
@@ -41,7 +41,7 @@ class SearchEvent(ListAPIView):
         data = json.loads(response.read())
 
         searchResults = []
-        searchItem = ""
+        searchItem = request.data.get('searchItem',None)
         semanticsUrl = "https://api.datamuse.com/words?ml=" + str(searchItem) + "&max=10"
         semanticsResponse = urllib.request.urlopen(semanticsUrl)
         semantics = json.loads(semanticsResponse.read())
