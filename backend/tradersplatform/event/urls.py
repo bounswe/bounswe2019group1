c75c641ef78d99c781ce905434a151319fbcd3c2
Ömer F. Toptaş <omer.toptas@boun.edu.tr>
2019-12-16 20:58:21 +0300

    Corrected urls.
    Corrected the url to obtain a parameter.

--- backend/tradersplatform/event/urls.py
+++ backend/tradersplatform/event/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
   url(r'^list/', views.EventCalendar.as_view(), name="update_de"),
-  url(r'^search/', views.SearchEvent.as_view(), name="update_de"),
+  url(r'^search/(?P<pk>[a-zA-Z]+)/', views.SearchEvent.as_view(), name="update_de"),
 ]
