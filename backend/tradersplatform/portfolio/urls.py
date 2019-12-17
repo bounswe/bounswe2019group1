ce51ac09c99ac6ac4ddb1b707227558fbf9ffa4e
abdullah-coskun <abdullah.coskun1@boun.edu.tr>
2019-12-17 23:38:01 +0300

    follow portfolio and get notification mechanism updated

--- backend/tradersplatform/portfolio/urls.py
+++ backend/tradersplatform/portfolio/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^createportfolio/', views.CreatePortfolioAPIView.as_view(), name="create"),
     url(r'^updateportfolio/(?P<pk>[0-9_]+)/', views.UpdatePortfolioAPIView.as_view(), name="create"),
+    url(r'^deleteportfolio/(?P<pk>[0-9_]+)/', views.DeletePortfolioAPIView.as_view(), name="create"),
     url(r'^retrieveportfolio/(?P<pk>[0-9_]+)/', views.RetrievePortfolioAPIView.as_view(), name="create"),
     url(r'^listportfolio/(?P<pk>[0-9_]+)/', views.ListPortfolioAPIView.as_view(), name="create"),
 ]
