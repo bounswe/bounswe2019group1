e0649f6a8963a8775edc9c973a4b83ac28b9c0b3
Ufuk Yilmaz <ufukyilmaz333@gmail.com>
2019-12-23 15:05:34 +0300

    Template for image annotation is added.

--- practice-app/react-front-end/src/components/Annotation/Annotation.js
+++ practice-app/react-front-end/src/components/Annotation/Annotation.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import Marker from 'react-icons/lib/ti/bookmark';
+import React from "react";
+import Marker from "react-icons/lib/ti/bookmark";
 
 export default function Annotation({ annotation, showAnnotation }) {
-    return(
-        <div className="annotation"
-             style={{left: annotation.target.x, top: annotation.target.y}}>
-            <Marker/>
-            {annotation.display &&
-            <div className="annotation-title">{annotation.body.text}</div>
-            }
-        </div>
-    );
+  return (
+    <div
+      className="annotation"
+      style={{ left: annotation.target.x, top: annotation.target.y }}
+    >
+      <Marker />
+      {annotation.display && (
+        <div className="annotation-title">{annotation.body.text}</div>
+      )}
+    </div>
+  );
 }
