c84071fa42fd99ac34228fd19f9401e2f5164030
ycerend <ycerendardagan@gmail.com>
2019-12-29 01:52:27 +0300

    .feat #425, endpoints are created and list model for all comments is implemented.

--- android/khajit/app/src/main/java/com/project/khajit_app/api/Api.kt
+++ android/khajit/app/src/main/java/com/project/khajit_app/api/Api.kt
@@ -230,4 +230,8 @@ interface Api {
     @POST("notification/createbuyorder/")
     fun createBuyOrder(@Body body: EquipmentBSOrderModel?):Call<EquipmentBSOrderResponse>
 
+    @Headers("Content-Type: application/json")
+    @GET("article-comment/list/{id}/" )
+    fun getArticleComments(@Path(value = "id", encoded = true) articleId: Int):Call<ListArticleCommentModel>
+
 }
---
+++ android/khajit/app/src/main/java/com/project/khajit_app/data/models/ListArticleCommentModel.kt
@@ -0,0 +1,3 @@
+package com.project.khajit_app.data.models
+
+data class ListArticleCommentModel(val result : List<ArticleCommentItem>)
