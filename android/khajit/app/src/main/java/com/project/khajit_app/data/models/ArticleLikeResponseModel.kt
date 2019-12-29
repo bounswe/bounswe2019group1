2a26ec20e6e083ce929032bb99be1bc21c66c9ba
ycerend <ycerendardagan@gmail.com>
2019-12-29 03:35:49 +0300

    .feat #424, article like/dislike data models and request models are implemented.

---
+++ android/khajit/app/src/main/java/com/project/khajit_app/data/models/ArticleLikeResponseModel.kt
@@ -0,0 +1,7 @@
+package com.project.khajit_app.data.models
+
+data class ArticleLikeResponseModel(val id: Int?,
+                                    val article : Int?,
+                                    val user : Int?,
+                                    val liked_date: String?)
+
